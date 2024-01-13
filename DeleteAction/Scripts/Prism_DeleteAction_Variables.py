ddf2a51a92b1478f8683d6d50016056415f9adef
Joshua Breckeen <86539171+AltaArts@users.noreply.github.com>
2024-01-13 14:27:30 -0500

    Up version

--- DeleteAction/Scripts/Prism_DeleteAction_Variables.py
+++ DeleteAction/Scripts/Prism_DeleteAction_Variables.py
@@ -45,7 +45,7 @@ import os
 
 class Prism_DeleteAction_Variables(object):
     def __init__(self, core, plugin):
-        self.version = "v2.0.3-0.1"
+        self.version = "v2.0.3-0.3"
         self.pluginName = "DeleteAction"
         self.pluginType = "Custom"
         self.platforms = ["Windows", "Linux", "Darwin"]
