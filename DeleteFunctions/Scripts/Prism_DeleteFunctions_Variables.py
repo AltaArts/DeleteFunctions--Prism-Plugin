344d632c8adf4b29974181f083fd169d44c648bc
Joshua Breckeen <86539171+AltaArts@users.noreply.github.com>
2024-02-11 14:55:12 -0500

    up version

--- DeleteFunctions/Scripts/Prism_DeleteFunctions_Variables.py
+++ DeleteFunctions/Scripts/Prism_DeleteFunctions_Variables.py
@@ -45,7 +45,7 @@ import os
 
 class Prism_DeleteFunctions_Variables(object):
     def __init__(self, core, plugin):
-        self.version = "v2.0.3-0.6.1beta"
+        self.version = "v2.0.3-1.0.0"
         self.pluginName = "DeleteFunctions"
         self.pluginType = "Custom"
         self.platforms = ["Windows", "Linux", "Darwin"]
